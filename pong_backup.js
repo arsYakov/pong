2749e37a613fd26f8bb883350eed9472ad1fc847
Ynot <150277533+arsYakov@users.noreply.github.com>
2024-07-03 14:44:36 -0400

    Designed updates

--- pong_backup.js
+++ pong_backup.js
@@ -1,10 +1,8 @@
 
-// Why do we need mypaddle initialized here?
 let myPaddle;
 let playerScore = 0;
 let pcScore = 0;
 
-// Gamebox grabs the html doc area to draw in and sets our game loop in the start function under interval. I think.
 let gameBox = {
     canvas: document.getElementById('pong'),
     height: 800,
@@ -20,12 +18,6 @@ let gameBox = {
     
 };
 
-// Start game gets the gamebox and calls start. 
-// It then makes the player and pc paddles, draws the score, and places a ball.
-// TODO: We need to clean up the creations by pulling out their arguments to named variables as this is super confusing.
-// TODO: We also need to make it so that the game resets when a score is had, and that the ball spawns in a random location.
-// TODO: Need to add a Start Screen.
-// TODO: Need to implement lives to end the game state.
 function startGame(){
     gameBox.start();
     myPaddle = new gameRect(gameBox.height/8, 10, 'rgba(218, 218, 216,1)', ((gameBox.width / 2) - ((gameBox.height/8)/2) ), gameBox.height-40 );
@@ -34,8 +26,7 @@ function startGame(){
     firstBall = new gameBall(200, 100, 5.5, 0, 2 * Math.PI, "litegrey");
 };
 
-// These are for the paddles, but could be named better
-// The compiler seems to think these should be classes instead of whatever I've got going on right now.
+
 function gameRect (width, height, color, x, y){
     this.width = width;
     this.height = height;
@@ -49,8 +40,7 @@ function gameRect (width, height, color, x, y){
         ctx.closePath();
     }
 };
-// This is to display the score. I guess thats fine for now
-// TODO: Change the location atleast. 
+
 function gameText (text, color, x, y){
     this.text = text;
     this.color = color;
@@ -68,11 +58,7 @@ function gameText (text, color, x, y){
     }
 };
 
-// This is a mess. The way we are solving the movement of the ball really needs to be reworked. 
-// TODO: To enable powers we will need a list of balls to begin with, and these balls will need to know how to move without this static
-//        enabled way we are currently doing it.
-// TODO: Add a gravity component that that balls will be attracted to. 
-// TODO: Fix the collision system to something that approches sanity. 
+
 function gameBall (x, y, radius, start, end, color){
     this.x = x;
     this.y = y;
@@ -114,7 +100,7 @@ function gameBall (x, y, radius, start, end, color){
         }
     }
 };
-// See notes on Game Ball. 
+
 function changeDirection(ball, direction){
     if (ball.xDirection == "positive" && direction == 'x'){
 
@@ -131,7 +117,6 @@ function changeDirection(ball, direction){
     }
 }
 
-// TODO: Figure out why the stuttering is happening
 function updateGame(){
     gameBox.clear();
     gameBox.frame += 1;
@@ -141,8 +126,6 @@ function updateGame(){
     pcAI(pcPaddle,firstBall)
 };
 
-// TODO: Add power menu
-// TODO: Figure out why the ball will pass thru the paddle sometimes
 function moveMe(event){
     // mousePos.labelX = event.clientX;
     // mousePos.labelY = event.clientY;
@@ -153,8 +136,7 @@ function moveMe(event){
     }
     myPaddle.update();
 };
-// See notes on game ball
-// TODO: Redo this whole thing. Garbage. 
+
 function makeBallTravel(ball){
     if (ball.xDirection == "positive" && ball.yDirection == "positive"){
         ball.x = ball.x + ball.xSpeed;
@@ -174,9 +156,7 @@ function makeBallTravel(ball){
     ball.update();
     ball.collision();
 };
-// This is very primative
-// TODO: Make this intelligent. Need to add an actual variance so that there is a change that the player can score.
-// TODO: Need to handle multiple game balls and add support for the AI to use Powers
+
 function pcAI(pcPaddle, firstBall){
     // console.log(pcPaddle, firstBall);
     if (gameBox.frame % 2 == 0){
@@ -188,9 +168,6 @@ function pcAI(pcPaddle, firstBall){
     pcPaddle.update();
 };
 
-// This is kinda ok.
-// TODO: Carry the score after ball respawns.
-// TODO: Change location.
 function score(side){
     if (side == 'pc'){
         displayScore.playerScore +=1;
